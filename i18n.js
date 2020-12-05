5b1d4fc1e99a2ef4d3602134f08de095f9a4e5bd
vadamk <volodymyr.kravch@gmail.com>
2020-12-05 18:35:14 +0700

    back static path

--- i18n.js
+++ i18n.js
@@ -2,15 +2,9 @@ const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
 
-console.log('process.env.ENV: ', process.env.NODE_ENV);
-
 module.exports = new NextI18Next({
   defaultLanguage: 'sw',
   otherLanguages: ['en'],
   localeSubpaths,
-  localePath: path.resolve(
-    process.env.NODE_ENV !== 'production'
-      ? './public/static/locales'
-      : './out_publish/static/locales'
-  )
+  localePath: path.resolve('./public/static/locales')
 });
