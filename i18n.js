e58b0662f0a239d1d2859fb7c547e1628e2bd467
vadamk <volodymyr.kravch@gmail.com>
2020-12-05 18:10:36 +0700

    add loger name

--- i18n.js
+++ i18n.js
@@ -2,7 +2,7 @@ const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
 
-console.log(path.resolve('./'));
+console.log('i18nPath', path.resolve('./'));
 
 module.exports = new NextI18Next({
   defaultLanguage: 'sw',
